a923bc2e52367166e07a2904cd0a73576d04a4ce
mgajda83 <47522674+mgajda83@users.noreply.github.com>
2020-08-08 19:08:54 +0200

    Update PSRenameDetector.ps1

--- Detectors/PSRenameDetector.ps1
+++ Detectors/PSRenameDetector.ps1
@@ -3,7 +3,16 @@ if(Test-Path -Path C:\WINDOWS\ServiceState\wmansvc\AutopilotDDSZTDFile.json) {
 	#Get Autopilot profile settings
 	$Autopilot = Get-Content C:\WINDOWS\ServiceState\wmansvc\AutopilotDDSZTDFile.json | ConvertFrom-Json
 
-	if($Autopilot.CloudAssignedDeviceName -match $env:COMPUTERNAME) {
+	#Check if current name match profile name
+	if($env:COMPUTERNAME -eq $Autopilot.CloudAssignedDeviceName) {
 		Write-Host "Yupi! Workstation name is correct: $($env:COMPUTERNAME) = $($Autopilot.CloudAssignedDeviceName)"
+	} else {
+		#Check if current name match profile name with prefix or it wait for reboot
+		$AppDeviceName = Get-Content "$($env:ProgramData)\Intune.ScriptLogs\PSRename\DeviceNane.tag" -ErrorAction SilentlyContinue
+		
+		if($AppDeviceName -like "$($Autopilot.CloudAssignedDeviceName)*")
+		{
+			Write-Host "Yupi! Workstation name seems correct: $AppDeviceName = $($Autopilot.CloudAssignedDeviceName)*"
+		}
 	}
 }
