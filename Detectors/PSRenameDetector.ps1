fcf57ea1df9e2ac23e8f140b35f05101c103b8e5
mgajda83 <47522674+mgajda83@users.noreply.github.com>
2020-08-08 06:23:14 +0200

    Update PSRenameDetector.ps1

--- Detectors/PSRenameDetector.ps1
+++ Detectors/PSRenameDetector.ps1
@@ -3,7 +3,7 @@ if(Test-Path -Path C:\WINDOWS\ServiceState\wmansvc\AutopilotDDSZTDFile.json) {
 	#Get Autopilot profile settings
 	$Autopilot = Get-Content C:\WINDOWS\ServiceState\wmansvc\AutopilotDDSZTDFile.json | ConvertFrom-Json
 
-	if($env:COMPUTERNAME -eq $Autopilot.CloudAssignedDeviceName) {
+	if($env:COMPUTERNAME -match $Autopilot.CloudAssignedDeviceName) {
 		Write-Host "Yupi! Workstation name is correct: $($env:COMPUTERNAME) = $($Autopilot.CloudAssignedDeviceName)"
 	}
 }
